8c59fcd45d92a7d06e72ebc2eca61994bb11d2bd
Nicole Tibay <neequole@gmail.com>
2018-04-11 16:19:32 +0800

    Create quote generator function for AJAX call

--- randomizer.py
+++ randomizer.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from google.oauth2 import service_account
 from googleapiclient import discovery
 
@@ -13,7 +13,6 @@ app = Flask(__name__)
 
 def connect():
     """ Creates Google spreadsheet service """
-    # TODO: Find way to save in Storage like Oauth2
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = discovery.build('sheets', 'v4', credentials=credentials)
@@ -29,6 +28,11 @@ def read_spreadsheet(service, id_, range_):
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/quote')
+def generate_quote():
     service = connect()
     spreadsheet_id = '1vh6cKhIPFQMq-F1vdviA5Rud6-fieBG-d8WaIbKW3jY'
     sheet_name = 'Form Responses 1!'
@@ -36,8 +40,8 @@ def index():
     total_range = sheet_name + 'D1:D1'
     total = read_spreadsheet(service, spreadsheet_id, total_range)
     questions_count = int(total[0][0])
-    row = random.randrange(2, questions_count+1)
+    row = random.randrange(2, questions_count + 1)
     # Get random row value
     question_range = '{}B{}:B{}'.format(sheet_name, row, row)
     question = read_spreadsheet(service, spreadsheet_id, question_range)
-    return render_template('index.html', random_question=question[0][0])
+    return jsonify(question[0][0])
