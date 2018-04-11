459c5a25a3f0209739aeff8d9490926a39f95698
Nicole Tibay <neequole@gmail.com>
2018-04-11 16:48:12 +0800

    Add loading info

--- static/script.js
+++ static/script.js
@@ -1,12 +1,19 @@
-function newQuote() {
+function sendHttpRequest(method, url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/quote', true);
+    xhr.open(method, url, true);
     xhr.send();
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById('quote_holder').innerHTML = this.responseText;
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
         }
     }
+}
+
+function newQuote() {
+    document.getElementById('quote_holder').innerHTML = 'Loading';
+    sendHttpRequest('GET', '/quote', function (response) {
+        document.getElementById('quote_holder').innerHTML = response;
+    });
 }
 
 newQuote();
