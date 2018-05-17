043d079be23a577d05a9481101bd4d851db3273c
Nicole Tibay <neequole@gmail.com>
2018-05-17 15:12:31 +0800

    Generate quote from csv file

--- randomizer.py
+++ randomizer.py
@@ -1,12 +1,16 @@
 import json
 import os
 import random
+import pathlib
+import csv
 
 from flask import Flask, render_template, jsonify, Response
 from google.oauth2 import service_account
 from googleapiclient import discovery
 from googleapiclient.http import MediaIoBaseDownload
 
+
+CSV_NAME = 'questions.csv'
 FILE_ID = '1vh6cKhIPFQMq-F1vdviA5Rud6-fieBG-d8WaIbKW3jY'
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 SERVICE_ACCOUNT_FILE = 'client_secret.json'
@@ -28,11 +32,23 @@ def connect():
     return service
 
 
-def read_spreadsheet(service, id_, range_):
-    """ Reads range from specified worksheet """
-    result = service.spreadsheets().values().get(
-        spreadsheetId=id_, range=range_).execute()
-    return result.get('values', [])
+def download_spreadsheet():
+    """ Downloads Google spreadsheet as CSV file """
+    service = connect()
+    request = service.files().export_media(fileId=FILE_ID, mimeType='text/csv')
+    with open(CSV_NAME, 'wb') as fh:
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+
+
+def read_csv():
+    """ Gets questions from CSV file """
+    with open(CSV_NAME, newline='') as fh:
+        reader = csv.DictReader(fh)
+        return list(reader)
 
 
 @app.route('/')
@@ -42,29 +58,17 @@ def index():
 
 @app.route('/quote')
 def generate_quote():
-    service = connect()
-    spreadsheet_id = '1vh6cKhIPFQMq-F1vdviA5Rud6-fieBG-d8WaIbKW3jY'
-    sheet_name = 'Form Responses 1!'
-    # Get random row number
-    total_range = sheet_name + 'D1:D1'
-    total = read_spreadsheet(service, spreadsheet_id, total_range)
-    questions_count = int(total[0][0])
-    row = random.randrange(2, questions_count + 2)  # 2: header + stop
-    # Get random row value
-    question_range = '{}B{}:B{}'.format(sheet_name, row, row)
-    question = read_spreadsheet(service, spreadsheet_id, question_range)
-    return jsonify(question[0][0])
+    # Download spreadsheet if not yet exists
+    path = pathlib.Path(CSV_NAME)
+    if not path.exists():
+        download_spreadsheet()
+    questions = read_csv()
+    # Generate random row number
+    row = random.randrange(0, len(questions))
+    return jsonify(questions[row]['Your question'])
 
 
 @app.route('/download')
-def download_sheet():
-    """ Downloads Google spreadsheet as CSV file """
-    service = connect()
-    request = service.files().export_media(fileId=FILE_ID, mimeType='text/csv')
-    with open('questions.csv', 'wb') as fh:
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print("Download %d%%." % int(status.progress() * 100))
+def download():
+    download_spreadsheet()
     return Response(status=200)
